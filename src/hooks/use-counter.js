93dc8eb34b35c44c92fae1b099d86bdc351d9b87
qpakr38 <zczc0819@gmail.com>
2022-09-05 16:37:25 +0900

    FEAT : 매개변수를 통한 좀더 유연한 Custom Hooks 적용하기

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,17 +1,22 @@
 import {useEffect, useState} from "react";
 
-const useCounter = () => {
-    const [counter, setCounter] = useState(0);
+const useCounter = (forwards = true) => {
+        const [counter, setCounter] = useState(0);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCounter((prevCounter) => prevCounter + 1);
-        }, 1000);
+        useEffect(() => {
+            const interval = setInterval(() => {
+                if (forwards) {
+                    setCounter((prevCounter) => prevCounter + 1);
+                } else {
+                    setCounter((prevCounter) => prevCounter - 1);
+                }
+            }, 1000);
 
-        return () => clearInterval(interval);
-    }, []);
+            return () => clearInterval(interval);
+        }, [forwards]);
 
-    return counter;
-};
+        return counter;
+    }
+;
 
 export default useCounter;
