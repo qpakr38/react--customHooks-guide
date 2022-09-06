fdba4a0e0f51d0a245354dc7250a3fd63f0968fc
qpakr38 <zczc0819@gmail.com>
2022-09-06 11:15:35 +0900

    FEAT: Custom Hooks으로 HTTP 요청하기

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from "./hooks/use-http";
 
 function App() {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
     const [tasks, setTasks] = useState([]);
 
-    const fetchTasks = async (taskText) => {
-        setIsLoading(true);
-        setError(null);
-        try {
-            const response = await fetch(
-                'https://react-http-f752e-default-rtdb.firebaseio.com/tasks.json'
-            );
+    const transformTasks = (tasksObject) => {
+        const loadedTasks = [];
 
-            if (!response.ok) {
-                throw new Error('Request failed!');
-            }
-
-            const data = await response.json();
-
-            const loadedTasks = [];
-
-            for (const taskKey in data) {
-                loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-            }
-
-            setTasks(loadedTasks);
-        } catch (err) {
-            setError(err.message || 'Something went wrong!');
+        for (const taskKey in tasksObject) {
+            loadedTasks.push({id: taskKey, text: tasksObject[taskKey].text});
         }
-        setIsLoading(false);
+        setTasks(loadedTasks);
     };
+    const {isLoading, error, sendRequest: fetchTasks} = useHttp(
+        {url: 'https://react-http-f752e-default-rtdb.firebaseio.com/tasks.json '}
+        , transformTasks
+    );
 
     useEffect(() => {
         fetchTasks();
@@ -45,7 +30,7 @@ function App() {
 
     return (
         <React.Fragment>
-            <NewTask onAddTask={taskAddHandler} />
+            <NewTask onAddTask={taskAddHandler}/>
             <Tasks
                 items={tasks}
                 loading={isLoading}
