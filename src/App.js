33b12e65409ee7c8728d704311dcaf3d435ebf13
qpakr38 <zczc0819@gmail.com>
2022-09-06 11:32:21 +0900

    REFACTORY : Custom Hooks으로 HTTP 요청하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
@@ -7,22 +7,19 @@ import useHttp from "./hooks/use-http";
 function App() {
     const [tasks, setTasks] = useState([]);
 
-    const transformTasks = (tasksObject) => {
-        const loadedTasks = [];
-
-        for (const taskKey in tasksObject) {
-            loadedTasks.push({id: taskKey, text: tasksObject[taskKey].text});
-        }
-        setTasks(loadedTasks);
-    };
-    const {isLoading, error, sendRequest: fetchTasks} = useHttp(
-        {url: 'https://react-http-f752e-default-rtdb.firebaseio.com/tasks.json '}
-        , transformTasks
-    );
+    const {isLoading, error, sendRequest: fetchTasks} = useHttp();
 
     useEffect(() => {
-        fetchTasks();
-    }, []);
+        const transformTasks = (tasksObject) => {
+            const loadedTasks = [];
+
+            for (const taskKey in tasksObject) {
+                loadedTasks.push({id: taskKey, text: tasksObject[taskKey].text});
+            }
+            setTasks(loadedTasks);
+        };
+        fetchTasks({ url:'https://react-http-f752e-default-rtdb.firebaseio.com/tasks.json '}, transformTasks);
+    }, [fetchTasks]);
 
     const taskAddHandler = (task) => {
         setTasks((prevTasks) => prevTasks.concat(task));
